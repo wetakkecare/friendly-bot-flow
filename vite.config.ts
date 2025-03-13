8f52d3c01c2472a7cbb4d4674e4cf0ff5b293038
wetakkecare <wetakkecare@gmail.com>
2025-03-13 18:57:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/friendly-bot-flow/',
   server: {
     host: "::",
     port: 8080,
@@ -19,8 +18,5 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    minify: false, // Disable minification for debugging
   },
 }));
