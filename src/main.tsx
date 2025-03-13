42315155aeaf7c961150feabfccaf6f280f93ff1
wetakkecare <wetakkecare@gmail.com>
2025-03-13 19:00:28 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Import HashRouter
-import App from './App.tsx';
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
