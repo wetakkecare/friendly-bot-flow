71e0bb54e1bdb6984831ecc37c16b7cda8135d9d
wetakkecare <wetakkecare@gmail.com>
2025-03-13 12:24:55 +0100

    Add files via upload

---
+++ docs/assets/browser-Tm2r_0oj.js
@@ -0,0 +1,14 @@
+import { g as getDefaultExportFromCjs } from "./index-DfqJzoyS.js";
+var browser = function() {
+  throw new Error(
+    "ws does not work in the browser. Browser clients must use the native WebSocket object"
+  );
+};
+const browser$1 = /* @__PURE__ */ getDefaultExportFromCjs(browser);
+const browser$2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: browser$1
+}, Symbol.toStringTag, { value: "Module" }));
+export {
+  browser$2 as b
+};
